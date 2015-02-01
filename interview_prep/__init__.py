b50e0810fc77b19b1f294b8e7c4c9dd7973faacc
stharrold <samuel.harrold@gmail.com>
2015-02-01 12:12:59 -0600

    __init__.py: only import future at top-level

--- interview_prep/__init__.py
+++ interview_prep/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+"""Code for interview questions.
 
+"""
+
+# Import future. Encapsulated scopes inherit.
 from __future__ import absolute_import, division, print_function
-
+# Import local packages.
 from . import fibonacci
--- interview_prep/fibonacci/__init__.py
+++ interview_prep/fibonacci/__init__.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
 """Calculate Fibonacci numbers.
 
 """
-
-from __future__ import absolute_import, print_function, division
 
 from . import fib
