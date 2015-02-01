20f2ae72a2bb556d35231f12b4007198e96dc696
stharrold <samuel.harrold@gmail.com>
2015-02-01 15:29:46 -0600

    fib.py: initialize fibs in own func

--- interview_prep/fibonacci/fib.py
+++ interview_prep/fibonacci/fib.py
@@ -2,9 +2,24 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-# Global variable to lookup computed Fibonnaci numbers.
-# Using a class requires extra code maintenance.
-fibs = []
+
+def _init_fibs():
+    """Semi-private function to initialize Fibonacci sequence for lookup.
+    Creates a global variable: fibs = [0, 1]
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    """
+    global fibs
+    fibs = [0, 1]
+    return None
+
 
 def fib_lookup(idx):
     """Compute Fibonacci number, looked up from an array.
@@ -21,10 +36,10 @@ def fib_lookup(idx):
     
     Notes
     -----
-    From [1]_: 
-    F_n = F_n-1 + F_n-2
     Time complexity is O(1) for in-memory lookup.
     Space complexity is O(phi^n), where phi is golden ratio, ~1.6.
+    From [1]_: 
+    F_n = F_n-1 + F_n-2
     
     References
     ----------
@@ -38,14 +53,21 @@ def fib_lookup(idx):
         raise ValueError(("`idx` must be an ``int`` >= 0:\n" +
                           "idx = {idx}").format(idx=idx))
     # Initialize fibs array and compute Fibonacci number.
+    if 'fibs' not in globals():
+        _init_fibs()
     if len(fibs) < 2:
-        fibs = [0, 1]
+        _init_fibs()
     if len(fibs) - 1 < idx:
         if idx > 1:
             # Note: Lookup F_n-1 before F_n-2 for easier trace.
             # Computations and memory access is equivalent.
-            fn = fib_lookup(idx-1) + fib_lookup(idx-2)
-            fibs.append(fn)
+            try:
+                logger.debug("Recursive call: fib_lookup(idx={idxn1}) + fib_lookup(idx={idxn2})".format(idxn1=idx-1,
+                                                                                                        idxn2=idx-2))
+                fn = fib_lookup(idx=idx-1) + fib_lookup(idx=idx-2)
+                fibs.append(fn)
+            except RuntimeError as err:
+                print(err, file=sys.stderr)
         else:
             raise AssertionError(("Program error. `fibs` must be initialized with [0,1]:\n" +
                                   "fibs = {fibs}").format(fibs=fibs))
@@ -71,10 +93,10 @@ def fib_recur(idx):
 
     Notes
     -----
+    Time complexity is O(phi^n), where phi is golden ratio, ~1.6.
+    Space complexity is O(1), for storing 2 ints.
     From [1]_: 
     F_n = F_n-1 + F_n-2
-    Time complexity is O(phi^n), where phi is golden ratio, ~1.6.
-    Space complexity is O(2), for 2 ints.
     
     References
     ----------
@@ -95,7 +117,12 @@ def fib_recur(idx):
     elif idx > 1:
         # Note: Lookup F_n-1 before F_n-2 for easier trace.
         # Computations and memory access is equivalent.
-        fn = fib_recur(idx-1) + fib_recur(idx-2)
+        try:
+            logger.debug("Recursive call: fib_lookup(idx={idxn1}) + fib_lookup(idx={idxn2})".format(idxn1=idx-1,
+                                                                                                    idxn2=idx-2))
+            fn = fib_recur(idx=idx-1) + fib_recur(idx=idx-2)
+        except RuntimeError as err:
+            print(err, file=sys.stderr)
     else:
         raise AssertionError(("Program error. `idx` must be an ``int`` >= 0:\n" +
                               "idx = {idx}").format(idx=idx))
