9b44824be8e1916dbbf55844ca289f291e49f134
stharrold <samuel.harrold@gmail.com>
2015-03-06 19:47:03 -0600

    interview_cake.py: get_products

--- interview_prep/interview_cake/interview_cake.py
+++ interview_prep/interview_cake/interview_cake.py
@@ -24,3 +24,26 @@ def calc_max_profit(prices):
     return max_profit
 
 
+def get_products_of_all_ints_except_at_index(ints):
+    """Get product of all ints save one for each idx.
+
+    Args:
+        ints: list of int
+        List of `int` factors.
+
+    Returns:
+        prods: list of int
+        List of `int` products.
+    """
+    idxs = xrange(len(ints))
+    prods = [1]*len(ints)
+    prod = 1
+    for idx in idxs:
+        prods[idx] = prod
+        prod *= ints[idx]
+    prod = 1
+    for idx in reversed(idxs):
+        prods[idx] *= prod
+        prod *= ints[idx]
+    return prods
+
