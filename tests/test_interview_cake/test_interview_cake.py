71ab4ce4312026fdafc39c44de47dee66ed2f601
stharrold <samuel.harrold@gmail.com>
2015-03-06 20:53:33 -0600

    test_interview_cake.py: test_get_highest_product

--- tests/test_interview_cake/test_interview_cake.py
+++ tests/test_interview_cake/test_interview_cake.py
@@ -1,6 +1,7 @@
 """Tests for interview_prep/interview_cake/interview_cake.py
 """
 
+
 def test_calc_max_profit(prices=[5.,4.,5.,3.,6.,5.,9.,8.,10.,2.,3.], max_profit=7.0):
     """pytest style test for calc_max_profit
     """
@@ -9,6 +10,7 @@ def test_calc_max_profit(prices=[5.,4.,5.,3.,6.,5.,9.,8.,10.,2.,3.], max_profit=
 
 test_calc_max_profit(prices=[5.,4.,5.,3.,6.,5.,9.,8.,10.,-20.,3.], max_profit=23.)
 
+
 def test_get_products_of_all_ints_except_at_index(ints=[1,7,3,4], prods=[7*3*4, 1*3*4, 1*7*4, 1*7*3]):
     """pytest style test
     """
@@ -16,7 +18,19 @@ def test_get_products_of_all_ints_except_at_index(ints=[1,7,3,4], prods=[7*3*4, 
     assert len(prod_pairs) == len(ints)
     return None
 
+
 test_get_products_of_all_ints_except_at_index()
 test_get_products_of_all_ints_except_at_index(ints=[0,0,0], prods=[0,0,0])
 test_get_products_of_all_ints_except_at_index(ints=[5], prods=[1])
 test_get_products_of_all_ints_except_at_index(ints=[], prods=[])
+
+
+def test_get_highest_product(ints=[0,3,2,5,4,-1,4], highest_product=80):
+    '''pytest style test for get_highest_product
+
+    '''
+    assert get_highest_product(ints=ints) == highest_product
+    return None
+
+
+test_get_highest_product()
