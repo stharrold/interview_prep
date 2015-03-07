3e581f26034191212618bc17f518814e73981ac5
stharrold <samuel.harrold@gmail.com>
2015-03-07 11:41:47 -0600

    tests: adding imports

---
+++ interview_prep/interview_cake/__init__.py
@@ -0,0 +1,5 @@
+"""My iterations of answers for Interview Cake.
+
+"""
+
+from . import interview_cake
--- tests/test_interview_cake/test_interview_cake.py
+++ tests/test_interview_cake/test_interview_cake.py
@@ -2,11 +2,18 @@
 """
 
 
+from __future__ import absolute_import, division, print_function
+import sys
+sys.path.insert(0, '.')
+import interview_prep as ip
+
+
 def test_calc_max_profit(prices=[5.,4.,5.,3.,6.,5.,9.,8.,10.,2.,3.], max_profit=7.0):
     """pytest style test for calc_max_profit
     """
-    assert calc_max_profit(prices) == max_profit
+    assert ip.interview_cake.calc_max_profit(prices) == max_profit
     return None
+
 
 test_calc_max_profit(prices=[5.,4.,5.,3.,6.,5.,9.,8.,10.,-20.,3.], max_profit=23.)
 
@@ -14,7 +21,8 @@ test_calc_max_profit(prices=[5.,4.,5.,3.,6.,5.,9.,8.,10.,-20.,3.], max_profit=23
 def test_get_products_of_all_ints_except_at_index(ints=[1,7,3,4], prods=[7*3*4, 1*3*4, 1*7*4, 1*7*3]):
     """pytest style test
     """
-    prod_pairs = filter(lambda tup: tup[0] == tup[1], zip(get_products_of_all_ints_except_at_index(ints), prods))
+    prod_pairs = filter(lambda tup: tup[0] == tup[1],
+                        zip(ip.interview_cake.get_products_of_all_ints_except_at_index(ints), prods))
     assert len(prod_pairs) == len(ints)
     return None
 
@@ -29,8 +37,7 @@ def test_get_highest_product(ints=[0,3,2,5,4,-1,4], highest_product=80):
     '''pytest style test for get_highest_product
 
     '''
-    assert get_highest_product(ints=ints) == highest_product
+    assert ip.interview_cake.get_highest_product(ints=ints) == highest_product
     return None
 
 
-test_get_highest_product()
