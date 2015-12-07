a21650aab11a28fe143132fa87169724bd51d085
stharrold <samuel.harrold@gmail.com>
2015-12-07 06:25:05 +0000

    probability.py: added q1

--- interview_prep/probability/probability.py
+++ interview_prep/probability/probability.py
@@ -19,11 +19,30 @@ References:
 import interview_prep.utils as utils
 
 
-def myfunc(myint:int=5) -> None:
+def q1(
+    pr_a:float=0.28, pr_b:float=0.29, pr_c:float=0.19,
+    pr_aib:float=0.14, pr_aic:float=0.12, pr_bic:float=0.10,
+    pr_aibic:float=0.08) -> None:
+    r"""Calculate P(not(A union B union C)).
+    
+    Args:
+        pr_[a,b,c] (float): [P(A), P(B), P(C)]
+        pr_[aib,aic,bic] (float): [P(A intr B), P(A intr C), P(B intr C)]
+        pr_aibic (float): P(A intr B intr C)
+    
+    Returns:
+        pr_naubuc (float): P(not(A union B union C))
+    
+    Notes:
+        * P(not(A union B union C)) = (1 - (
+            P(A) + P(B) + P(C) - P(A intr B) - P(A intr C)
+            - P(B intr C) + P(A intr B intr C)))
+
+    """
     # Check arguments.
     utils.check_arguments(
-        antns=myfunc.__annotations__,
+        antns=q1.__annotations__,
         lcls=locals())
-    return myint
-
-
+    # Calculate union complement.
+    pr_naubuc = 1.0 - (pr_a + pr_b + pr_c - pr_aib - pr_aic - pr_bic + pr_aibic)
+    return pr_naubuc
--- tests/test_probability/test_probability.py
+++ tests/test_probability/test_probability.py
@@ -9,12 +9,22 @@ r"""Pytests for interview_prep/interview_prep/probability/probability.py
 import os
 import sys
 # Import installed packages.
+import numpy as np
 # Import local packages.
 sys.path.insert(0, os.path.curdir)
 import interview_prep.probability.probability as pr
 
 
-def test_myfunc() -> None:
-    test_myint = pr.myfunc()
-    assert test_myint == 5
+def test_q1(
+    pr_a:float=0.28, pr_b:float=0.29, pr_c:float=0.19,
+    pr_aib:float=0.14, pr_aic:float=0.12, pr_bic:float=0.10,
+    pr_aibic:float=0.08, ref_pr_naubuc=0.52) -> None:
+    r"""Pytest for q1.
+    
+    """
+    test_pr_naubuc = pr.q1(
+        pr_a=pr_a, pr_b=pr_b, pr_c=pr_c,
+        pr_aib=pr_aib, pr_aic=pr_aic, pr_bic=pr_bic,
+        pr_aibic=pr_aibic)
+    assert np.isclose(ref_pr_naubuc, test_pr_naubuc)
     return None
